957c867ec40cbdd878b5dcdc85424e8430b1c206
Rodrigues <rdina@audible.com>
2025-03-19 22:39:03 -0400

    Update

--- Starter_Files/QuickDBD-export.sql
+++ Starter_Files/QuickDBD-export.sql
@@ -3,27 +3,27 @@
 
 
 CREATE TABLE "campaign" (
-    "cf_id" VARCHAR(50)   NOT NULL,
-    "contact_id" VARCHAR(50)   NOT NULL,
-    "company_name" VARCHAR(50)   NOT NULL,
-    "description" VARCHAR(50)   NOT NULL,
-    "goal" FLOAT   NOT NULL,
-    "pledged" FLOAT   NOT NULL,
+    "cf_id" INT   NOT NULL,
+    "contact_id" INT   NOT NULL,
+    "company_name" VARCHAR(100)   NOT NULL,
+    "description" TEXT   NOT NULL,
+    "goal" NUMERIC(10,2)   NOT NULL,
+    "pledged" NUMERIC(10,2)   NOT NULL,
     "outcome" VARCHAR(50)   NOT NULL,
     "backers_count" INT   NOT NULL,
-    "country" VARCHAR(50)   NOT NULL,
-    "currency" VARCHAR(50)   NOT NULL,
+    "country" VARCHAR(10)   NOT NULL,
+    "currency" VARCHAR(10)   NOT NULL,
     "launch_date" DATE   NOT NULL,
     "end_date" DATE   NOT NULL,
-    "category_id" VARCHAR(50)   NOT NULL,
-    "subcategory_id" VARCHAR(50)   NOT NULL,
+    "category_id" VARCHAR(10)   NOT NULL,
+    "subcategory_id" VARCHAR(10)   NOT NULL,
     CONSTRAINT "pk_campaign" PRIMARY KEY (
         "cf_id"
      )
 );
 
 CREATE TABLE "category" (
-    "category_id" VARCHAR(50)   NOT NULL,
+    "category_id" VARCHAR(10)   NOT NULL,
     "category" VARCHAR(50)   NOT NULL,
     CONSTRAINT "pk_category" PRIMARY KEY (
         "category_id"
@@ -31,7 +31,7 @@ CREATE TABLE "category" (
 );
 
 CREATE TABLE "subcategory" (
-    "subcategory_id" VARCHAR(50)   NOT NULL,
+    "subcategory_id" VARCHAR(10)   NOT NULL,
     "subcategory" VARCHAR(50)   NOT NULL,
     CONSTRAINT "pk_subcategory" PRIMARY KEY (
         "subcategory_id"
@@ -39,7 +39,7 @@ CREATE TABLE "subcategory" (
 );
 
 CREATE TABLE "contacts" (
-    "contact_id" VARCHAR(50)   NOT NULL,
+    "contact_id" INT   NOT NULL,
     "first_name" VARCHAR(50)   NOT NULL,
     "last_name" VARCHAR(50)   NOT NULL,
     "email" VARCHAR(100)   NOT NULL,
